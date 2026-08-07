10a8545559968c5a09d52d05842461e5d97340d0
jaypasnagasai <jay662466@gmail.com>
2026-08-07 08:40:00 -0700

    add files

--- api/login.ts
+++ api/login.ts
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import { VercelRequest, VercelResponse } from '@vercel/node'
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' })
   }
